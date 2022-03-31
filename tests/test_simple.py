14d60ebfd2082f464b48bdcd4527409207c78fc5
andriygav <andriy.graboviy@mail.ru>
2022-03-31 10:09:20 +0300

    try fix

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -9,3 +9,8 @@ def test_dataset():
     dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
 
     assert len(dataset.X) == len(dataset.y)
+
+def test_cv():
+    Cs, accuracy, parameters = cv_parameters(dataset.X, dataset.y)
+
+    assert len(Cs) == len(accuracy) == len(parameters)
