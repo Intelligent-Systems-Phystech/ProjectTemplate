06ae70df7a848eaf63de651871c3571dc8fead9e
andriygav <andriy.graboviy@mail.ru>
2022-03-31 10:13:44 +0300

    try fix

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -11,15 +11,19 @@ def test_dataset():
     assert len(dataset.X) == len(dataset.y)
 
 def test_trainer():
+    dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
+
     trainer = Trainer(
         LogisticRegression(penalty='l1', solver='saga', C=1.0),
         dataset.X, dataset.y,
     )
-
     trainer.train()
+    
     assert trainer.eval(output_dict=True)['accuracy'] == 0.96
 
 def test_cv():
+    dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
+
     Cs, accuracy, parameters = cv_parameters(dataset.X, dataset.y)
 
     assert len(Cs) == len(accuracy) == len(parameters)
