ec2d66c45a01d4b2c241d0b257fa183e414dae17
andriygav <andriy.graboviy@mail.ru>
2022-03-31 09:59:10 +0300

    add test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,6 +1,11 @@
 import mylib
+from mylib.train import cv_parameters, Trainer, SyntheticBernuliDataset
 
 def test_sample():
     assert 0 == 0
 
 
+def test_dataset():
+    dataset = SyntheticBernuliDataset(n=10, m=100, seed=42)
+
+    assert len(dataset.X) == len(dataset.y)
